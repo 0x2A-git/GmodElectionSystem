9b5381ff3400d8677a7a43fe6a45baa544287252
0x2A <20931885+0x2A-git@users.noreply.github.com>
2021-03-22 18:25:20 +0100

    Update sh_state.lua

--- elections/lua/autorun/sh_state.lua
+++ elections/lua/autorun/sh_state.lua
@@ -1,6 +1,6 @@
 --[[
 
-Copyright 2020 Zozo832 ( https://github.com/Zozo832 )
+Copyright 2021 0x2A ( https://github.com/0x2A-git )
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
